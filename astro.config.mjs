f7503426ce53a157c897e1ae82b3356464051cd5
Felipe Gallo <felipegallo94@gmail.com>
2026-02-05 20:55:34 -0500

    Set Spanish as default language

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://nexo.com',
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'es',
     locales: ['en', 'es'],
     routing: {
       prefixDefaultLocale: true,
